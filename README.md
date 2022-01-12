93efebeb4948f4629765b5c23d2fb3be01beeb42
SylleoYr <SylleoYr@users.noreply.github.com>
2022-01-12 12:55:21 +0800

    initial version

---
+++ README.md
@@ -0,0 +1,20 @@
+# RSSHub Access Code Generator
+
+An access code generator written in Python for [RSSHub](https://github.com/DIYgod/RSSHub).
+
+See [here](https://docs.rsshub.app/install/#pei-zhi-fang-wen-kong-zhi-pei-zhi) for details.
+
+![screenshot_1](./assets/screenshot_1.png)
+
+## Run
+
+1. Install the [PySimpleGUI](https://github.com/PySimpleGUI/PySimpleGUI) before running: `pip install pysimplegui` or `pip3 install pysimplegui`.
+2. `python main.py`
+
+## Thanks
+
+- [PySimpleGUI/PySimpleGUI](https://github.com/PySimpleGUI/PySimpleGUI)
+
+## License
+
+[BSD-3-Clause](./LICENSE)
---
+++ main.py
@@ -0,0 +1,92 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Author: Sylleo
+Version: 0.0.1
+Date: 2022-01-12 08:56:36
+LastEditTime: 2022-01-12 11:31:21
+LastEditors: Sylleo
+Description: An access code generator for RSSHub.
+FilePath: /rsshub-access-gen/main.py
+Environment: Python=3.9.7  PySimpleGUI=4.56.0
+Copyright (C) 2022 Sylleo. All rights reserved.
+'''
+
+from hashlib import md5
+import PySimpleGUI as sg
+
+sg.theme('DarkGreen6')
+
+
+def main():
+    layout = [
+        [sg.Text('RSSHub URL', size=(10, 1)), sg.InputText(default_text='https://rsshub.app', key='-URL-', focus=True, size=(65, 1))],
+        [sg.Text('Access Key', size=(10, 1)), sg.InputText(key='-KEY-', size=(65, 1))],
+        [sg.Text('Route', size=(10, 1)), sg.InputText(key='-ROUTE-', size=(65, 1))],
+        [sg.Text('Access Code', size=(10, 1)), sg.Multiline(key='-OUTPUT-', disabled=True, auto_refresh=True, rstrip=True, size=(65, 10))],
+        [
+            sg.Button('Ok', bind_return_key=True, size=(10, 1), pad=((0, 20), (0, 0))),
+            sg.Button('Copy Access Code', button_color='green'),
+            sg.Button('Copy Feed URL with Code', button_color='green'),
+            sg.Button('Copy Feed URL with Key', button_color='green')
+        ]
+    ]
+
+    window = sg.Window('RSSHub Access Code Generator', layout=layout)
+
+    while True:
+        event, values = window.read()
+
+        if event == sg.WIN_CLOSED:
+            break
+        if event == 'Ok':
+            url = values['-URL-'] or 'https://rsshub.app'
+            key = values['-KEY-']
+            route = values['-ROUTE-']
+
+            if not route:
+                window['-OUTPUT-'].update('Err: The route is empty!', text_color_for_value='red')
+                continue
+            
+            # Compute access code
+            md5encoder = md5()
+            md5encoder.update((route + key).encode('utf-8'))
+            code = md5encoder.hexdigest()
+            # Get feed urls
+            feed_url_code = url + route + '?code=' + code
+            feed_url_key = url + route + '?key=' + key
+            
+            window['-OUTPUT-'].update('Access Code:\n')
+            window['-OUTPUT-'].update(code, append=True, text_color_for_value='blue')
+            window['-OUTPUT-'].update('\nFeed URL with Code:\n', append=True)
+            window['-OUTPUT-'].update(feed_url_code, append=True, text_color_for_value='blue')
+            window['-OUTPUT-'].update('\nFeed URL with Key:\n', append=True)
+            window['-OUTPUT-'].update(feed_url_key, append=True, text_color_for_value='blue')
+        if event == 'Copy Access Code':
+            output = window['-OUTPUT-'].get().splitlines()
+
+            if output:
+                # print(output)
+                # code = output[1]
+                sg.clipboard_set(output[1])
+        if event == 'Copy Feed URL with Code':
+            output = window['-OUTPUT-'].get().splitlines()
+
+            if output:
+                # print(output)
+                # feed_url_code = output[1]
+                sg.clipboard_set(output[3])
+        if event == 'Copy Feed URL with Key':
+            output = window['-OUTPUT-'].get().splitlines()
+
+            if output:
+                # print(output)
+                # feed_url_key = output[1]
+                sg.clipboard_set(output[5])
+
+    window.close()
+
+
+if __name__ == '__main__':
+    main()
